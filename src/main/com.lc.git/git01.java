80a057a953ccd046f6635bc3817a6c599c251007
Lc <scutlc@163.com>
2022-01-06 23:10:04 +0800

    second commit from idea

--- src/main/com.lc.git/git01.java
+++ src/main/com.lc.git/git01.java
@@ -3,5 +3,6 @@ package main.com.lc.git;
 public class git01 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world 2!");
     }
 }
