0cb2f74082fe0c908ce864a290d9b6652db0f0a4
Lc <scutlc@163.com>
2022-01-06 23:21:01 +0800

    master second commit

--- src/main/com.lc.git/git01.java
+++ src/main/com.lc.git/git01.java
@@ -2,9 +2,10 @@ package main.com.lc.git;
 
 public class git01 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello master222!");
         System.out.println("Hello world 2!");
         System.out.println("hot-fit111");
         System.out.println("hot-fit111");
+        System.out.println("master222");
     }
 }
