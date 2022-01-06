1b816f4fc69352d24452e3917769c4e11a529c48
Lc <scutlc@163.com>
2022-01-06 23:17:08 +0800

    hot-fit first commit

--- src/main/com.lc.git/git01.java
+++ src/main/com.lc.git/git01.java
@@ -4,5 +4,7 @@ public class git01 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world 2!");
+        System.out.println("hot-fit111");
+        System.out.println("hot-fit111");
     }
 }
